984aa736ad8b7bb29ccec6e16f293ba89d49d0a6
Frank Hundley <mariner105@msn.com>
2021-10-23 14:50:28 -0700

    Added test resources

--- src/test/java/com/mariner105/unittesting/unittesting/UnitTestingApplicationTests.java
+++ src/test/java/com/mariner105/unittesting/unittesting/UnitTestingApplicationTests.java
@@ -2,8 +2,13 @@ package com.mariner105.unittesting.unittesting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+// Specify which property file(s) to use for this test class
+// They can be used to override other properties of the same names
+// that are in the file
+@TestPropertySource(locations={"classpath:test-configuration.yml"})
 class UnitTestingApplicationTests {
 
 	@Test
--- src/test/java/com/mariner105/unittesting/unittesting/controller/ItemControllerIT.java
+++ src/test/java/com/mariner105/unittesting/unittesting/controller/ItemControllerIT.java
@@ -1,14 +1,19 @@
 package com.mariner105.unittesting.unittesting.controller;
 
+import com.mariner105.unittesting.unittesting.data.ItemRepository;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ItemControllerIT {
+
+    @MockBean
+    private ItemRepository repository;
 
     public static final boolean STRICT_FALSE = false;
     public final String URL_ALL_ITEMS = "/all-items-from-database";
