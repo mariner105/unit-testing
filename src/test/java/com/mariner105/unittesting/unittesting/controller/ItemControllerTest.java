821e8f558431ba0f911d5c00ca40eb6635319995
Frank Hundley <mariner105@msn.com>
2021-10-23 07:51:59 -0700

    Added ItemControllerTest

--- src/test/java/com/mariner105/unittesting/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/mariner105/unittesting/unittesting/controller/ItemControllerTest.java
@@ -1,5 +1,44 @@
 package com.mariner105.unittesting.unittesting.controller;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemController.class)
 class ItemControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void dummyItem_basic() throws Exception {
+        //call "/dummy-item"
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/dummy-item")
+                .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request);
+
+        //verify that the response contains the expected JSON
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}"))
+                .andReturn();
+
+        //We don't need this  assertion now that we have added the andExpect calls above
+        //assertEquals("Hello World", result.getResponse().getContentAsString());
+
+    }
 }
