b26840e99369e909f600c5ba320c6e14e076c16d
Frank Hundley <mariner105@msn.com>
2021-10-22 17:21:46 -0700

    Initial files

---
+++ src/test/java/com/mariner105/unittesting/unittesting/UnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariner105.unittesting.unittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
