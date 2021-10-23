9884d2b0522e18eef6f65cafa4bfaa0aaa79b05b
Frank Hundley <mariner105@msn.com>
2021-10-23 14:25:41 -0700

    Added more fields to EXPECTED_RESPONSE_JSON

--- src/test/java/com/mariner105/unittesting/unittesting/controller/ItemControllerIT.java
+++ src/test/java/com/mariner105/unittesting/unittesting/controller/ItemControllerIT.java
@@ -13,7 +13,9 @@ public class ItemControllerIT {
     public static final boolean STRICT_FALSE = false;
     public final String URL_ALL_ITEMS = "/all-items-from-database";
     public final String EXPECTED_RESPONSE_JSON =
-            "[{id:10001}, {id:10002}, {id:10003}]";
+            "[{id:10001,name:Item1,price:10,quantity:20}," +
+                "{id:10002,name:Item2,price:5,quantity:10}," +
+                "{id:10003,name:Item3,price:15,quantity:2}]";
 
     @Autowired
     private TestRestTemplate restTemplate;
