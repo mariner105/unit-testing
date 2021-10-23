75ec01eb26cae7eca1f71abb212cb4ea119c13ef
Frank Hundley <mariner105@msn.com>
2021-10-23 16:54:41 -0700

    Fixed ItemControllerIT

--- src/test/java/com/mariner105/unittesting/unittesting/controller/ItemControllerIT.java
+++ src/test/java/com/mariner105/unittesting/unittesting/controller/ItemControllerIT.java
@@ -1,19 +1,17 @@
 package com.mariner105.unittesting.unittesting.controller;
 
-import com.mariner105.unittesting.unittesting.data.ItemRepository;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ItemControllerIT {
 
-    @MockBean
-    private ItemRepository repository;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     public static final boolean STRICT_FALSE = false;
     public final String URL_ALL_ITEMS = "/all-items-from-database";
@@ -22,8 +20,6 @@ public class ItemControllerIT {
                 "{id:10002,name:Item2,price:5,quantity:10}," +
                 "{id:10003,name:Item3,price:15,quantity:2}]";
 
-    @Autowired
-    private TestRestTemplate restTemplate;
 
     @Test
     public void contextLoads() throws JSONException {
