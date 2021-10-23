462394ea48e49bf4685b2826f111e524f25a76d2
Frank Hundley <mariner105@msn.com>
2021-10-23 15:14:05 -0700

    Refactored to use functional programming for the sum

--- src/main/java/com/mariner105/unittesting/unittesting/business/SomeBusinessImpl.java
+++ src/main/java/com/mariner105/unittesting/unittesting/business/SomeBusinessImpl.java
@@ -2,6 +2,8 @@ package com.mariner105.unittesting.unittesting.business;
 
 import com.mariner105.unittesting.unittesting.data.SomeDataService;
 
+import java.util.Arrays;
+
 public class SomeBusinessImpl {
 
     private  SomeDataService dataService;
@@ -11,17 +13,26 @@ public class SomeBusinessImpl {
         this.dataService = dataService;
     }
 
+    /**
+     * Calculate the some using functional programming.
+     * @param data An array of integers whose sum be returned
+     * @return sum of the integers
+     */
+    public int calculateSum(int[] data) {
+        return Arrays.stream(data).reduce(Integer::sum).orElse(0);
+    }
+
     public int calculateSumUsingDataService() {
         int[] data = dataService.retrieveAllData();
-        int sum = 0;
-        for (int value : data) {
-            sum += value;
-        }
+//        int sum = 0;
+//        for (int value : data) {
+//            sum += value;
+//        }
 
         // If the following was a real method call then we could use verify tests to check that it was executed as
         // expected
         // someDataService.storeSum(sum);
-        return sum;
+        return calculateSum(data);
     }
 
 }
